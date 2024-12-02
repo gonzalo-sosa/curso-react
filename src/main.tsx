abb8679333d68ec62c087e0354306303f1315bf1
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-02 15:55:10 -0300

    No usados, elimnados

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./fontawesome.js";
 import App from "./App.tsx";
-import Dashboard from "./components/routes/admin/dashboard.jsx";
-import Posts from "./components/routes/posts.jsx";
-import Products from "./components/routes/products.jsx";
-import NotFound from "./components/routes/notFound.jsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
